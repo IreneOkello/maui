a2739c84768e95b4ae8e763edfedc4d45b3a1241
Irene <ireneatienoo@syncfusion.com>
2024-11-22 10:15:22 +0300

    EnableUITest Bugzilla/Bugzilla60524.cs

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Bugzilla/Bugzilla60524.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Bugzilla/Bugzilla60524.cs
@@ -1,25 +1,23 @@
-﻿#if TEST_FAILS_ON_WINDOWS
-using NUnit.Framework;
-using NUnit.Framework.Legacy;
+﻿using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
-namespace Microsoft.Maui.TestCases.Tests.Issues;
-
-public class Bugzilla60524 : _IssuesUITest
+namespace Microsoft.Maui.TestCases.Tests.Issues
 {
-	public Bugzilla60524(TestDevice testDevice) : base(testDevice)
+	public class Bugzilla60524 : _IssuesUITest
 	{
-	}
+		public Bugzilla60524(TestDevice testDevice) : base(testDevice)
+		{
+		}
 
-	public override string Issue => "NRE when rendering ListView with grouping enabled and HasUnevenRows set to true";
+		public override string Issue => "NRE when rendering ListView with grouping enabled and HasUnevenRows set to true";
 
-	[Test]
-	[Category(UITestCategories.ListView)]
-	[FailsOnIOSWhenRunningOnXamarinUITest]
-	public void Bugzilla60524Test()
-	{
-		App.WaitForElement("Group 1");
+		[Test]
+		[Category(UITestCategories.ListView)]
+		public void Bugzilla60524Test()
+		{
+			App.WaitForElement("GroupedListView");
+			App.WaitForElement("TestLabel");
+		}
 	}
 }
-#endif
